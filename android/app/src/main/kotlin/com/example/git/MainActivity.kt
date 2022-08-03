3c18cd4d6dc6fe3cde2f7c4a91859fb7415ea71c
Suda-C <kiran1234c@gmail.com>
2022-08-02 20:14:29 -0500

    hi

--- android/app/src/main/kotlin/com/example/git/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.git
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo000000000000000 888888888888888',
+      title: 'Flutter Demo000000000000000 888888888888888',g
       theme: ThemeData(
         // This is the theme of your application.
         //
